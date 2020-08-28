6352dbf0fc262985b64ff1f7c502f991f233f9e8
tangkun <962163607@qq.com>
2020-08-28 10:31:48 +0800

    完成个人中心开发

--- assets/js/user/user_avatar.js
+++ assets/js/user/user_avatar.js
@@ -14,54 +14,53 @@ $(function() {
   // 1.3 创建裁剪区域
   $image.cropper(options)
 
-  // 为上传按钮绑定点击事件
-  $('#btnChooseImage').on('click', function() {
-    $('#file').click()
+  $("#btnChooseImage").on("click", function () { 
+    $("#file").click();
   })
 
-  // 为文件选择框绑定 change 事件
-  $('#file').on('change', function(e) {
-    // 获取用户选择的文件
-    var filelist = e.target.files
-    if (filelist.length === 0) {
-      return layer.msg('请选择照片！')
+  $("#file").on("change", function (e) { 
+    var fileList = e.target.files;
+    
+    if (fileList.length === 0) {
+      alert(12)
+      return layer.msg("请选择照片！")
     }
-
-    // 1. 拿到用户选择的文件
-    var file = e.target.files[0]
-    // 2. 将文件，转化为路径
-    var imgURL = URL.createObjectURL(file)
-    // 3. 重新初始化裁剪区域
+    var file = e.target.files[0];
+    var imgURL = URL.createObjectURL(file);
     $image
-      .cropper('destroy') // 销毁旧的裁剪区域
-      .attr('src', imgURL) // 重新设置图片路径
-      .cropper(options) // 重新初始化裁剪区域
+      .cropper("destroy")
+      .attr("src", imgURL)
+    .cropper(options)
+
+
+
   })
 
-  // 为确定按钮，绑定点击事件
-  $('#btnUpload').on('click', function() {
-    // 1. 要拿到用户裁剪之后的头像
+  $("#btnUpload").on("click", function () { 
     var dataURL = $image
-      .cropper('getCroppedCanvas', {
-        // 创建一个 Canvas 画布
-        width: 100,
-        height: 100
-      })
-      .toDataURL('image/png') // 将 Canvas 画布上的内容，转化为 base64 格式的字符串
-    // 2. 调用接口，把头像上传到服务器
+    .cropper('getCroppedCanvas', {
+      // 创建一个 Canvas 画布
+      width: 100,
+      height: 100
+    })
+      .toDataURL('image/png')
+    
     $.ajax({
-      method: 'POST',
-      url: '/my/update/avatar',
-      data: {
-        avatar: dataURL
+      type: "post",
+      url: "http://ajax.frontend.itheima.net/my/update/avatar",
+      headers: {
+        Authorization:localStorage.getItem("token")
       },
-      success: function(res) {
-        if (res.status !== 0) {
-          return layer.msg('更换头像失败！')
+      data: {
+        avatar:dataURL
+      },
+      success: function (response) {
+        if (response.status !== 0) {
+          return layer.msg("上传失败")
         }
-        layer.msg('更换头像成功！')
+        layer.msg("上传成功")
         window.parent.getUserInfo()
-      }
+       }
     })
   })
 })
--- assets/js/user/user_info.js
+++ assets/js/user/user_info.js
@@ -1,57 +1,66 @@
-$(function() {
-  var form = layui.form
-  var layer = layui.layer
-
+$(function () { 
+  var form = layui.form;
+  var layer = layui.layer;
   form.verify({
-    nickname: function(value) {
+    nickname: function (value) { 
       if (value.length > 6) {
-        return '昵称长度必须在 1 ~ 6 个字符之间！'
-      }
+        return "昵称长度必须1-6个字符之间！"
+       }
     }
   })
 
-  initUserInfo()
+  initUserInfo();
 
-  // 初始化用户的基本信息
-  function initUserInfo() {
+  $("#chongzhi").on("click", function (e) { 
+    e.preventDefault();
+    initUserInfo();
+  })
+
+  $(".layui-form").on("submit", function (e) { 
+    e.preventDefault();
+    var formData = $("#formData").serialize();
+    // console.log(formData)
+
+    $(document).ajaxStart(function () {
+      console.log("请求开始了");
+    });
     $.ajax({
-      method: 'GET',
-      url: '/my/userinfo',
-      success: function(res) {
-        if (res.status !== 0) {
-          return layer.msg('获取用户信息失败！')
+      type: "post",
+      url: "http://ajax.frontend.itheima.net/my/userinfo",
+      headers: {
+        Authorization:localStorage.getItem("token")
+      },
+      data: formData,
+      success: function (response) { 
+        if (response.status !== 0) {
+          return layer.msg("更新用户信息失败")
         }
-        // console.log(res)
-        // 调用 form.val() 快速为表单赋值
-        form.val('formUserInfo', res.data)
+        layer.msg("更新用户信息成功！")
+        window.parent.getUserInfo();
+      }
+
+      })
+  })
+  
+
+  $(document).ajaxStop(function () { 
+    console.log("请求结束了");
+  })
+  function initUserInfo() { 
+    $.ajax({
+      method: "get",
+      url: "http://ajax.frontend.itheima.net/my/userinfo",
+      headers: {
+        Authorization:localStorage.getItem("token")||""
+      },
+      success: function (response) { 
+        // console.log(response)
+        if (response.status !== 0) {
+          return layer.msg("获取失败")
+        }
+        console.log(response);
+        form.val("formUserInfo",response.data)
       }
     })
   }
-
-  // 重置表单的数据
-  $('#btnReset').on('click', function(e) {
-    // 阻止表单的默认重置行为
-    e.preventDefault()
-    initUserInfo()
-  })
-
-  // 监听表单的提交事件
-  $('.layui-form').on('submit', function(e) {
-    // 阻止表单的默认提交行为
-    e.preventDefault()
-    // 发起 ajax 数据请求
-    $.ajax({
-      method: 'POST',
-      url: '/my/userinfo',
-      data: $(this).serialize(),
-      success: function(res) {
-        if (res.status !== 0) {
-          return layer.msg('更新用户信息失败！')
-        }
-        layer.msg('更新用户信息成功！')
-        // 调用父页面中的方法，重新渲染用户的头像和用户的信息
-        window.parent.getUserInfo()
-      }
-    })
-  })
 })
